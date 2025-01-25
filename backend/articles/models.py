e46fb7a1d2bc555d0a463dbb786a2a68595f7005
Cho Seonghan <chosnhn1@gmail.com>
2025-01-25 18:24:38 +0900

    add models in app articles

--- backend/articles/models.py
+++ backend/articles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    contents = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+class Comment(models.Model):
+    contents = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    

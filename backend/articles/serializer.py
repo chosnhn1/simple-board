c8fceecbad0f1faeec08fdbdfa3cfae05a710d8e
Cho Seonghan <chosnhn1@gmail.com>
2025-02-04 16:10:17 +0900

    add comment related views in app article

--- backend/articles/serializer.py
+++ backend/articles/serializer.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Article, Comment
 
 class ArticleListSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = Article
         fields = ['id', 'title', 'contents', 'author', 'created_at', 'updated_at', 'is_notice']
@@ -14,6 +14,7 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
+        read_only_fields = ['id', 'author', 'article', 'created_at']
 
 class ArticleDetailSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True, read_only=True)
--- backend/articles/views.py
+++ backend/articles/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import get_object_or_404, get_list_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .models import Article
-from .serializer import ArticleDetailSerializer, ArticleListSerializer
+from .models import Article, Comment
+from .serializer import ArticleDetailSerializer, ArticleListSerializer, CommentSerializer
 
 # Create your views here.
 @api_view(['GET', 'POST'])
@@ -47,3 +47,25 @@ def article_detail(request, pk):
             return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
         
     return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+@api_view(['POST'])
+def comment_list(request, article_pk):
+    if request.user.is_authenticated:
+        article = get_object_or_404(Article, pk=article_pk)
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(author=request.user, article=article)
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+@api_view(['DELETE'])
+def comment_detail(request, article_pk, comment_pk):
+    if request.user.is_authenticated:
+        comment = get_object_or_404(Comment, pk=comment_pk)
+        if request.user == comment.author:
+            comment.delete()
+            return Response(status=status.HTTP_202_ACCEPTED)
+            
+    else:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)

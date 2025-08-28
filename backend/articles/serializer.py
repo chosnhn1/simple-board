cdf0bc624ce8bf92ba26c703f20607b45245122a
Cho Seonghan <chosnhn1@gmail.com>
2025-08-28 17:17:33 +0900

    change serializer on articles

--- backend/articles/serializer.py
+++ backend/articles/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Article, Comment
+from accounts.serializer import UserDetailSerializer
 
 class ArticleListSerializer(serializers.ModelSerializer):
     author = serializers.StringRelatedField()
@@ -11,7 +12,8 @@ class ArticleListSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    
+    author = UserDetailSerializer()
+
     class Meta:
         model = Comment
         fields = '__all__'
@@ -19,7 +21,8 @@ class CommentSerializer(serializers.ModelSerializer):
 
 class ArticleDetailSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True, read_only=True)
-
+    author = UserDetailSerializer()
+    
     class Meta:
         model = Article
         fields = ['id', 'title', 'content', 'created_at', 'updated_at', 'is_notice', 'author', 'comments']
